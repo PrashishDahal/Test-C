0c463905bd5f4c5942f40f3474e8251a3c3cc1a3
PrashishD <prashishd7@gmail.com>
2021-04-09 14:25:51 +0545

    prints shapes- triangle and square
    * used printf to print the character which results  into shapes-

---
+++ first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	printf("   /|     ______     \n");
+	printf("  / |    |      |\n");
+    printf(" /  |    |      | \n");
+    printf("/___|    |______|\n");
+return 0;	
+}	
+ 
+			 
+	
