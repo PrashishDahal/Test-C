a998bd57d641b4ba9080b4ef0b93d3bc6d008458
PrashishD <prashishd7@gmail.com>
2021-04-22 16:15:14 +0545

    C math functions

---
+++ numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+ int main()
+ {
+     
+     printf("%f\n", pow(2, 10) );
+     printf ("%f\n", ceil(36.2));
+     printf("%f\n", floor(39.7));
+     printf("%f\n", sqrt(49));
+     
+
+  return 0;   
+     
+ }
