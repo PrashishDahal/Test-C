553984defbce79df9b8ed7d30fc2331cfc116d08
lioninthewild <prashishd7@gmail.com>
2021-06-29 18:52:01 +0545

    correct solution

--- implementing-algorithms/fulltriangle.c
+++ implementing-algorithms/fulltriangle.c
@@ -2,12 +2,20 @@
 
 int main()
 {
-	int i, j, n;
+	int i, j, k, n;
 	printf("enter how many rows you want: ");
 	scanf("%d", &n);
 	for (i = 1; i <= n; i++)
 	{
-		for (j = 1; j <= i; j += 2)
+		for (k = 1; k <= (n - i); k++)
+		{
+			printf(" ");
+		}
+		for (j = 1; j <= i; j++)
+		{
+			printf("*");
+		}
+		for (j = 2; j <= i; j++)
 		{
 			printf("*");
 		}
