84be6d9747eb1a0268dfed9f905270580f233654
Prashish Dahal <dahal7prashish@gmail.com>
2021-07-29 23:42:33 +0545

    improvised

--- implementing-algorithms/recursion/ex2.c
+++ implementing-algorithms/recursion/ex2.c
@@ -2,26 +2,38 @@
 int fib(int);
 int main()
 {
-	int n;
+	int n, value;
 	printf("Enter the position of the number you want; ");
 	scanf("%d", &n);
 	printf("considering the series starts from 0 as.....0,1,1,2,3,5,8,....\n");
-	printf("The required number of %dth position is %d\n.", n, fib(n));
+	value = fib(n);
+	if (value < 0)
+	{
+		printf("Enter a valid position.\n");
+	}
+	else
+	{
+		printf("The required number of %dth position is %d\n.", n, value);
+	}
 	return 0;
 }
 int fib(int x)
 {
-	if (x >= 2)
+	if (x == 0)
 	{
-		return fib(x - 1) + fib(x - 2);
+		return -1;
 	}
+
 	else if (x == 1)
+	{
+		return 0;
+	}
+	else if (x == 2)
 	{
 		return 1;
 	}
-
-	// else
-	// {
-	// return 0;
-	// }
+	else
+	{
+		return fib(x - 1) + fib(x - 2);
+	}
 }
